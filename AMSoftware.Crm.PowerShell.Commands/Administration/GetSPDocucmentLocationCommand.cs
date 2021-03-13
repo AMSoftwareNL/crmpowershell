04602e85d0d26c644e8d63a8fd45babf6901d5e6
Richie <rb1@outlook.at>
2021-03-13 21:11:36 +0100

    New Command: Get-CrmSPDocLoc (#23)
    * Introduction of new Admininstration Cmdlet get-crmspdocloc
    
    this cmdlet enabled you to pass the recordid of a docloc and it will return the absolute URL of the corresponding sharepoint-folder. Further it accepts the recordid of any entity to record the according absolute-urls of all related doclocs...
    
    * Delete storage.ide-shm
    
    * Delete storage.ide
    
    * Delete storage.ide-wal
    
    * Update AMSoftware.Crm.PowerShell.Commands/Administration/GetSPDocLoc.cs
    
    Co-authored-by: Arjan Meskers <AMSoftwareNL@users.noreply.github.com>
    
    * added parameter validation
    added outputtype
    
    * - refactored to two parametersets
    - removed Entity-Parameter
    - renamed Verb as suggested
    - added documentation skeleton file
    
    Co-authored-by: Richard Bosse <richard.bosse@orbis.at>
    Co-authored-by: Arjan Meskers <AMSoftwareNL@users.noreply.github.com>

---
+++ AMSoftware.Crm.PowerShell.Commands/Administration/GetSPDocucmentLocationCommand.cs
@@ -0,0 +1,75 @@
+﻿using AMSoftware.Crm.PowerShell.Common;
+using AMSoftware.Crm.PowerShell.Common.ArgumentCompleters;
+using AMSoftware.Crm.PowerShell.Common.Repositories;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMSoftware.Crm.PowerShell.Commands.Administration
+{
+    [Cmdlet(VerbsCommon.Get, "CrmSPDocumentLocation", HelpUri = HelpUrlConstants.GetSPDocumentLocationHelpUrl, SupportsPaging = true, DefaultParameterSetName = GetBySharePointDocumentLocationId)]
+    [OutputType(typeof(RetrieveAbsoluteAndSiteCollectionUrlResponse))]
+    public class GetSPDocucmentLocationCommand : CrmOrganizationCmdlet
+    {
+        private const string GetBySharePointDocumentLocationId = "GetBySharePointDocumentLocationId";
+
+        [Parameter(Mandatory = true, Position = 0,ParameterSetName = GetBySharePointDocumentLocationId)]
+        [ValidateNotNullOrEmpty]
+        [Alias("Id")]
+        public Guid[] SharePointDocumentLocationId { get; set; }
+
+        private const string GetByRegardingObjectId = "GetByRegardingObjectId";
+
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = GetByRegardingObjectId)]
+        [ValidateNotNullOrEmpty]
+        public Guid[] RegardingObjectId { get; set; }
+
+        private ContentRepository _repository = new ContentRepository();
+        protected override void ExecuteCmdlet()
+        {
+            base.ExecuteCmdlet();
+
+            switch (this.ParameterSetName)
+            {
+                case GetBySharePointDocumentLocationId:
+                    {
+                        foreach (var id in SharePointDocumentLocationId)
+                        {
+                            RetrieveUrlById(id);
+                        }
+                        break;
+                    }
+                case GetByRegardingObjectId:
+                    {
+                        foreach (var id in RegardingObjectId)
+                        {
+                            QueryByAttribute query = new QueryByAttribute("sharepointdocumentlocation");
+                            query.AddAttributeValue("regardingobjectid", id);
+                            var doclocresult = _repository.Get(query);
+                            foreach (var docloc in doclocresult)
+                            {
+                                RetrieveUrlById(docloc.Id);
+                            }
+                        }
+                        break;
+                    }
+            }
+        }
+
+        private void RetrieveUrlById(Guid id)
+        {
+            var sourceUrlRequest = new RetrieveAbsoluteAndSiteCollectionUrlRequest
+            {
+                Target = new EntityReference("sharepointdocumentlocation", id)
+            };
+            var sourceUrlResponse = (RetrieveAbsoluteAndSiteCollectionUrlResponse)CrmContext.OrganizationProxy.Execute(sourceUrlRequest);
+            WriteObject(sourceUrlResponse);
+        }
+    }
+}
---
+++ docs/Get-CrmSPDocumentLocation.md
@@ -0,0 +1,45 @@
+---
+external help file: AMSoftware.Crm.Powershell.Commands.dll-Help.xml
+Module Name: AMSoftware.Crm
+online version: https://github.com/AMSoftwareNL/crmpowershell/blob/master/docs/Get-CrmSPDocumentLocation.md
+schema: 2.0.0
+---
+
+# Get-CrmSPDocumentLocation
+
+## SYNOPSIS
+!! Documentation incomplete, will be completed when code is final ;) 
+
+## SYNTAX
+
+
+## DESCRIPTION
+Get the SharePoint-Url for a CRM-record
+
+
+
+## EXAMPLES
+AbsoluteUrl       : https://yourtenant.sharepoint.com/sites/Dynamics365/account/Account1_a3a7377657e6e811a953000d3a4640d2
+SiteCollectionUrl : https://yourtenant.sharepoint.com/sites/Dynamics365
+ResponseName      : RetrieveAbsoluteAndSiteCollectionUrl
+Results           : {[SiteCollectionUrl, https://yourtenant.sharepoint.com/sites/Dynamics365], [AbsoluteUrl, 
+                    https://yourtenant.sharepoint.com/sites/Dynamics365/account/Account1_a3a7377657e6e811a953000d3a4640d2]}
+ExtensionData     : System.Runtime.Serialization.ExtensionDataObject
+
+## PARAMETERS
+
+
+### CommonParameters
+This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable. For more information, see about_CommonParameters (http://go.microsoft.com/fwlink/?LinkID=113216).
+
+## INPUTS
+
+### System.Guid
+## OUTPUTS
+
+### System.Object
+## NOTES
+
+## RELATED LINKS
+
+Todo: link msdoc to return object 
